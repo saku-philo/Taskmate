200ca648fb1ff5cd447b06b05d2c61e9da2ee955
hiroyuki_sakurada <sanno.phill@gmail.com>
2019-04-16 16:26:53 +0900

    アプリ日本語部分共通化、Time zoneを日本時間に設定

--- config/application.rb
+++ config/application.rb
@@ -10,7 +10,12 @@ module Taskmate
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
-
+    # I18n setting
+    config.i18n.available_locales = [:ja, :en]
+    config.i18n.default_locale = :ja
+    config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '**', '*.{rb,yml}').to_s]
+    # Time zone setting
+    config.time_zone = 'Asia/Tokyo'
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
